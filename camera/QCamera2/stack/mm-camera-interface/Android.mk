53cd578ef1eeceb5ca739ff86d13ba7487f33c19
pbteja100 <pbteja100@gmail.com>
2021-10-23 22:01:12 +0530

    [temp]msm8937-common: temporary change till sources are done

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -42,7 +42,8 @@ LOCAL_CFLAGS += -D_ANDROID_ -DQCAMERA_REDEFINE_LOG
 LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/inc \
     $(LOCAL_PATH)/../common \
-    hardware/qcom/media/media/mm-core/inc \
+    $(LOCAL_PATH)/../../../../../../../hardware/qcom-caf/msm8996/media/mm-core/inc/OMX_QCOMExtns.h \
+    hardware/qcom-caf/msm8996/media/mm-core/inc \
     system/media/camera/include \
 
 LOCAL_CFLAGS += -DCAMERA_ION_HEAP_ID=ION_IOMMU_HEAP_ID
