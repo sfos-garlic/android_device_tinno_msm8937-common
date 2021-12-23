761ee41110022c3b4a2f7afae1a09071dc3a6cac
pbteja100 <pbteja100@gmail.com>
2021-12-23 05:56:21 +0530

    Revert "msm8937-common: Switch to QCRIL"
    This reverts commit 71ec3dd6a7318398b068fe94924d6f32f4a5a750.

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -225,8 +225,9 @@ TARGET_RECOVERY_FSTAB := $(COMMON_PATH)/rootdir/etc/fstab.qcom
 endif
 
 # RIL
-TARGET_USES_OLD_MNC_FORMAT := true
-ENABLE_VENDOR_RIL_SERVICE := true
+#TARGET_RIL_VARIANT := caf
+#TARGET_USES_OLD_MNC_FORMAT := true
+#DISABLE_RILD_OEM_HOOK := true
 
 # SELinux
 include device/qcom/sepolicy-legacy-um/SEPolicy.mk
--- msm8937.mk
+++ msm8937.mk
@@ -279,7 +279,6 @@ PRODUCT_PACKAGES += \
     init.recovery.qcom.rc \
     init.msm.usb.configfs.rc \
     init.qcom.usb.sh \
-    init.insmod.sh \
     ueventd.qcom.rc \
     fstab.qcom
 
@@ -429,8 +428,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0 \
     android.hardware.radio.deprecated@1.0.vendor \
-    android.hardware.secure_element@1.2 \
-    android.hardware.secure_element@1.2.vendor \
+    android.hardware.secure_element@1.0 \
+    android.hardware.secure_element@1.0.vendor \
     libcnefeatureconfig \
     librmnetctl \
     libxml2
