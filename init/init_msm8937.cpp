d3f2ca1c13215309509fcecd34c36ffe46d6344e
pbteja100 <pbteja100@gmail.com>
2022-04-22 11:19:29 +0530

    msm8937-common: init: Update libinit * 'persist/alarm/powerOffAlarmSet' is no longer needed by poweroffalarm app so remove it.

--- init/init_msm8937.cpp
+++ init/init_msm8937.cpp
@@ -1,5 +1,7 @@
 /*
+   Copyright (C) 2007, The Android Open Source Project
    Copyright (c) 2016, The CyanogenMod Project
+   Copyright (c) 2017, The LineageOS Project
    Redistribution and use in source and binary forms, with or without
    modification, are permitted provided that the following conditions are
    met:
@@ -27,53 +29,34 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
+
+#include <sys/stat.h>
+#include <sys/types.h>
 
 #include <android-base/file.h>
+#include <android-base/logging.h>
 #include <android-base/properties.h>
 #include <android-base/strings.h>
 
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
 #include "vendor_init.h"
+#include "property_service.h"
 
-using android::base::GetProperty;
-using android::base::ReadFileToString;
-using android::init::property_set;
 
-static void init_alarm_boot_properties()
+void property_override(char const prop[], char const value[], bool add = true)
 {
-    char const *boot_reason_file = "/proc/sys/kernel/boot_reason";
-    char const *power_off_alarm_file = "/persist/alarm/powerOffAlarmSet";
-    std::string boot_reason;
-    std::string power_off_alarm;
-    std::string reboot_reason = GetProperty("ro.boot.alarmboot", "");
+    auto pi = (prop_info *) __system_property_find(prop);
 
-    if (ReadFileToString(boot_reason_file, &boot_reason)
-            && ReadFileToString(power_off_alarm_file, &power_off_alarm)) {
-        /*
-         * Setup ro.alarm_boot value to true when it is RTC triggered boot up
-         * For existing PMIC chips, the following mapping applies
-         * for the value of boot_reason:
-         *
-         * 0 -> unknown
-         * 1 -> hard reset
-         * 2 -> sudden momentary power loss (SMPL)
-         * 3 -> real time clock (RTC)
-         * 4 -> DC charger inserted
-         * 5 -> USB charger insertd
-         * 6 -> PON1 pin toggled (for secondary PMICs)
-         * 7 -> CBLPWR_N pin toggled (for external power supply)
-         * 8 -> KPDPWR_N pin toggled (power key pressed)
-         */
-         if ((Trim(boot_reason) == "3" || reboot_reason == "true")
-                 && Trim(power_off_alarm) == "1") {
-             property_set("ro.alarm_boot", "true");
-         } else {
-             property_set("ro.alarm_boot", "false");
-         }
+    if (pi != nullptr) {
+         __system_property_update(pi, value, strlen(value));
+    } else if (add) {
+        __system_property_add(prop, strlen(prop), value, strlen(value));
     }
 }
 
 void vendor_load_properties()
 {
-    init_alarm_boot_properties();
+	LOG(INFO) << "Loading vendor specific properties";
 }
